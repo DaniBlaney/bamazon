8a3199bb09173b472db94a732bc59d1f9310d578
Danielle Blaney <daniblaney@gmail.com>
2019-05-08 15:29:55 -0400

    userPrompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,4 +16,43 @@ var connection = mysql.createConnection({
   database: 'bamazon'
 });
 
+//prompts user what id and how many
 
+function userPurchase(){
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'item_id',
+      message: 'Please enter the Items ID which you would like to purchase.',
+      filter: Number
+
+    },
+    {
+      type: 'input',
+      name: 'quanity',
+      message: 'How many do you need?',
+      filter: Number
+    }
+  ]).then(function(input){
+      var item = input.item_id;
+      var quanity = input.quanity;
+
+      var queryId = 'SELECT * FROM products WHERE?';
+
+      connection.query(queryId, {item_id: item}, function(err, data){
+        if (err) throw err;
+
+        else if (data.length === 0){
+          console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+          displayInventory();
+        }
+        else {
+          var productData = data[0];
+          console.log('productData.stock_quantity = ' + productData.stock_quantity);
+          
+        }
+      })
+
+  })
+}
